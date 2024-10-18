acbf9337a52735c1ae855a334ccae3f73c3f8c76
Nakul Saini <kanhasaini2017@gmail.com>
2024-10-18 22:49:28 +0530

    correction

--- module/setup.py
+++
@@ -1,5 +0,0 @@
-#setup.py 
-
-from setuptools import setup
-
-setup(name = "numb3rs", version = "1.0", description = "Validation Tool for IPv4 Adresses", author = "HackerKing5128", py_modules = ["numb3rs"], )
